2fec514cb186cbadebbf4fcee07eb2559c10da11
opilane <opilane@HP-CZC2349HTG>
2026-02-04 09:25:53 +0200

    Add project files.

---
+++ TARpe24MobiilirakendusedAiron/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TARpe24MobiilirakendusedAiron
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
